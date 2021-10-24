10c12185a7d8dec6a0bb20714429e3e1994be033
JSDevL <dev.arfath@gmail.com>
2021-10-24 19:23:51 +0530

    ADDED: AppComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,113 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {StudentService} from './services/domain/student-service/student.service';
+import {DebugElement} from '@angular/core';
+import {of, throwError} from 'rxjs';
+import {By} from '@angular/platform-browser';
+import {BrowserTestingModule} from '@angular/platform-browser/testing';
+import {FormsModule} from '@angular/forms';
+import SpyObj = jasmine.SpyObj;
+import createSpyObj = jasmine.createSpyObj;
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let de: DebugElement;
+  let studentService: StudentService;
+
+  const studentServiceSpyObj: SpyObj<StudentService> = createSpyObj('studentServiceSpyObj', [
+    'fetchStudents', 'deleteStudent', 'addStudent'
+  ]);
+
   beforeEach(async(() => {
+    studentServiceSpyObj.fetchStudents.and.returnValue(of([]));
+
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        BrowserTestingModule,
+        FormsModule
       ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+      providers: [
+        {provide: StudentService, useValue: studentServiceSpyObj}
+      ]
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        de = fixture.debugElement;
+        studentService = TestBed.get(StudentService);
+      });
   }));
 
+  afterAll(() => {
+    TestBed.resetTestingModule();
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'abcc'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('abcc');
+  it('should populate table with fetched students', () => {
+    // given
+    studentServiceSpyObj.fetchStudents.and.returnValue(of([
+      {name: 'Arfath', roll: '14BCA4225'},
+      {name: 'Arfath1', roll: '14BCA4225_1'}
+    ]));
+
+    // when
+    fixture.detectChanges();
+
+    // then
+    expect(de.queryAll(By.css('.student-row')).length).toEqual(2);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should populate table with fetched students when new student is added', () => {
+    // given
+    studentServiceSpyObj.addStudent.and.returnValue(of({
+      name: 'Arfath', roll: '14BCA4225'
+    }));
+    studentServiceSpyObj.fetchStudents.and.returnValue(of([
+      {name: 'Arfath', roll: '14BCA4225'},
+    ]));
+
+    // when
+    component.createStudent('Arfath', '14BCA4225');
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('abcc app is running!');
+
+    // then
+    expect(de.queryAll(By.css('.student-row')).length).toEqual(1);
+  });
+
+  it('should alert user when attempting to add duplicate Student', () => {
+    // given
+    studentServiceSpyObj.addStudent.and.returnValue(throwError({
+      error: {message: 'Some Error'}
+    }));
+    spyOn(window, 'alert').and.stub();
+
+    // when
+    component.createStudent('Arfath', '14BCA4225');
+    fixture.detectChanges();
+
+    // then
+    expect(window.alert).toHaveBeenCalledWith('Some Error');
+  });
+
+  it('should populate table with fetched students when student is deleted', () => {
+    // given
+    studentServiceSpyObj.deleteStudent.and.returnValue(of(undefined));
+    studentServiceSpyObj.fetchStudents.and.returnValue(of([]));
+    spyOn(window, 'confirm').and.returnValue(true);
+
+    // when
+    component.deleteStudent(1);
+    fixture.detectChanges();
+
+    // then
+    expect(de.queryAll(By.css('.student-row')).length).toEqual(0);
   });
 });

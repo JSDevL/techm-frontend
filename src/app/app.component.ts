bf9797d0f918d34de00e1e9b5aee75f9878dfccf
JSDevL <dev.arfath@gmail.com>
2021-10-24 19:12:08 +0530

    FIXED: provided BasePathProviderImplService to the container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {StudentService} from './services/domain/students-service/student.service';
+import {StudentService} from './services/domain/student-service/student.service';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {Student} from './domain/model/student';
+import {catchError, switchMap, tap} from 'rxjs/operators';
 
 // import {ModalDirective} from 'angular-bootstrap-md';
 
@@ -10,48 +13,57 @@ import {StudentService} from './services/domain/students-service/student.service
 })
 export class AppComponent implements OnInit {
 
-  student$;
-  user = {
+  student$: Observable<Student[]>;
+  user: Student = {
     name: '',
     roll: '',
   };
 
+  private fetchStudents$ = new BehaviorSubject(this);
 
   constructor(private studentService: StudentService) {
-
   }
 
+  ngOnInit() {
+    this.student$ = this.fetchStudents$.pipe(
+      switchMap(() => this.studentService.fetchStudents())
+    );
+  }
 
-
-  deleteStudent(id) {
+  deleteStudent(id: number) {
     /*
     Complete this function to delete student and upon deletion of existing student based on the id of the student,
     It should prompt for a comfirmation for deletion and
     the updated list should be reflected in the UI
     */
+    const confirmation = confirm(`Are you sure you want to delete Student(${id})`);
 
-
+    if (confirmation) {
+      this.withStateChangeTrigger(
+        this.studentService.deleteStudent(id)
+      );
+    }
   }
 
-  createStudent(name, roll) {
+  createStudent(name: string, roll: string) {
     /*
     Complete this function to create new student taking parameters as name and roll
     and upon addition of new student, he should be reflected in the UI
     */
-
-
+    this.withStateChangeTrigger(
+      this.studentService.addStudent(name, roll)
+    );
   }
 
-
-  ngOnInit() {
-
-    this.student$ = this.studentService.fetchStudent();
-
-
+  private withStateChangeTrigger(observable: Observable<any>) {
+    observable.pipe(
+      catchError((e) => {
+        alert(e && e.error && e.error.message || e.message || e);
+        throw e;
+      }),
+      tap(() => this.fetchStudents$.next(this))
+    ).subscribe();
   }
-
-
-
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {StudentService} from './services/domain/student-service/student.service';
 import {FormsModule} from '@angular/forms';
+import {InjectionTokens} from './services/injection-tokens';
+import {BasePathProviderImplService} from './services/core/base-path-provider/base-path-provider-impl.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import {FormsModule} from '@angular/forms';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [StudentService],
+  providers: [
+    StudentService,
+    {provide: InjectionTokens.BASE_PATH_PROVIDER_SERVICE, useClass: BasePathProviderImplService}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
